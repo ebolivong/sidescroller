07fa475cc2a4dff544d8e57164537a8b713f87f6
Graphic <graphic@SOD-SJH-516-05M.local>
2015-10-23 15:51:16 -0400

    determine image scrolling variable

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {	
 
+
+	// width of image - width of window 
 	var horizontal = $('.hills').width() - $(window).width();
+	// height of window(body) + remaining width of image 
 	var vertical = $(window).height() + horizontal;
+	// sets the body height to the vertical for scrolling
 	$('body').css('height', vertical);
 	// console.log(px);
 
-	// height of window(body) + remaining width of image 
-	// width of image - width of window 
+	$(window).scroll(function() {
+		//determine how many pixels from the top is scrolled
+		var px = $(window).scrollTop();
 
+		$('.hills').css('left', - px)
+
+	});
 
 });
